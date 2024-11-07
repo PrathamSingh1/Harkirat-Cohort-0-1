8c884f19f5a8e8c7f88cbd6aa9ce21a250adc442
Pratham Singh <prathamjii36@gmail.com>
2024-11-07 23:29:12 +0530

    Learning HTTP

---
+++ Week_2/Week 2.1 Revision Of Promises, Async JS/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let a = 1;
+console.log(a);
+
+fs.readFile("a.txt", "utf-8", function(err, data) {
+    console.log("data read from the file is ");
+    console.log(data);
+})
+
+let ans = 0;
+for (let i = 0; i<100; i++) {
+    ans = ans + i;
+}
+console.log(ans);
