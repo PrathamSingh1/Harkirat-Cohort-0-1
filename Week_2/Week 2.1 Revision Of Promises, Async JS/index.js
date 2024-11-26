98697699eaa8fcce39098d02f1feb3411b20664e
Pratham Singh <prathamjii36@gmail.com>
2024-11-26 20:05:42 +0530

    revision

--- Week_2/Week 2.1 Revision Of Promises, Async JS/index.js
+++ Week_2/Week 2.1 Revision Of Promises, Async JS/index.js
@@ -12,3 +12,49 @@ for (let i = 0; i<100; i++) {
     ans = ans + i;
 }
 console.log(ans);
+
+
+// do a network call
+// sleep/wait for some time 
+// read a file
+// database call
+
+
+// Above these are the only 4 cases where you need to do a asynchronous calls other than that you just write synchronous code throughtout JavaScript.
+
+
+
+
+// myOwnsettimeout function
+
+function myOwnSetTimeout(fn, duration) {
+    setTimeout(fn, duration);
+}
+
+myOwnSetTimeout(function() {
+    console.log("done!")
+}, 1000)
+
+console.log("hello from outside")
+
+
+
+
+
+
+
+// promise function
+
+function fn(resolve) {
+
+    let a = 0;
+    for(i = 1; i<100; i++) {
+        a = a + i;
+    }
+    resolve(a);
+}
+const p = new Promise(fn);
+
+p.then(function(){
+    console.log("hello folks");
+})
