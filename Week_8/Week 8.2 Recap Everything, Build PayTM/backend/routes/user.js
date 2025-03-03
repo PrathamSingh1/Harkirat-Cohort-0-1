9ae3e3734043aaf86df63fceccb0ee0400a50d7d
Pratham Singh <prathamjii36@gmail.com>
2025-03-03 15:22:00 +0000

    simple paytm backend

--- Week_8/Week 8.2 Recap Everything, Build PayTM/backend/routes/user.js
+++ Week_8/Week 8.2 Recap Everything, Build PayTM/backend/routes/user.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 const { User } = require("../db");
 const JWT_SECRET = require("../config");
+const { authmiddleware } = require("../midddleware");
+
 
 const signupSchema = zod.object({
     username: zod.string().email(),
@@ -12,6 +14,8 @@ const signupSchema = zod.object({
     firstName: zod.string().min(2, "First name must be at least 2 characters"),
     lastName: zod.string().min(2, "Last name must be at least 2 characters")
 });
+
+
 
 router.post("/signup", async (req, res) => {
     const body = req.body;
@@ -42,6 +46,134 @@ router.post("/signup", async (req, res) => {
         token: token
     })
 
+});
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+});
+
+router.post("/signin", async (req, res) => {
+    const signinResult = signinBody.safeParse(req.body)
+    if (!signinResult.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+    
+})
+
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body)
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+		await User.updateOne({ _id: req.userId }, req.body);
+	
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
+
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+    const userId = user._id;
+
+		/// ----- Create new account ------
+
+    await Account.create({
+        userId,
+        balance: 1 + Math.random() * 10000
+    })
+
+		/// -----  ------
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
 })
 
 module.exports = router;
